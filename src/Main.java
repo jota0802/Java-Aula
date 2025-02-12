aaa520cf9d5254db8a159865aa908d77ba50e703
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-12 10:22:12 -0300

    aba

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,16 @@ public class Main {
         int soma;
 
         //declarando Scanner
-        Scanner sc = new Scanner(System.in);
+        Scanner scannerString = new Scanner(System.in);
+        Scanner scannerNumber = new Scanner(System.in);
 
         //declarando numero 1
         System.out.println("Insira o primeiro numero:");
-        numero1 = sc.nextInt();
+        numero1 = scannerNumber.nextInt();
 
         //declarando numero 2
         System.out.println("Insira o segundo numero:");
-        numero2 = sc.nextInt();
+        numero2 = scannerNumber.nextInt();
 
         //soma
         soma = numero1 + numero2;
@@ -28,20 +29,20 @@ public class Main {
 
         //declarando Nome
         System.out.println("Insira seu Nome");
-        String nome = sc.nextLine();
+        String nome = scannerString.nextLine();
 
         //declarando Idade
         System.out.println("Insira sua idade");
-        int idade = sc.nextInt();
+        int idade = scannerNumber.nextInt();
 
         //declarando Endereco
         System.out.println("Insira seu Endereco");
-        String endereco = sc.next();
+        String endereco = scannerString.nextLine();
 
         System.out.println("Olá " + nome + '!');
         System.out.println("endereço: " + endereco);
         System.out.println("idade: " + idade);
 
-        //porque o nextLine nao funciona no ultimo? pq se n tem nenhum comando depois nao funciona, ou porque tem string e int juntos
+        //porque o nextLine nao funciona no ultimo? pq se n tem nenhum comando depois nao funciona, ou porque tem string e int juntos, dai por isso criamos dois scanners diferentes
     }
 }
