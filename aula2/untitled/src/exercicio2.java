f84d1301521de1cfaf822c48574712af3fbf6f90
João Victor de Andrade Franco <jotinha@MacBook-Pro-de-Johnny.local>
2025-02-13 00:17:42 -0300

    ex2

--- aula2/untitled/src/exercicio2.java
+++ aula2/untitled/src/exercicio2.java
@@ -6,8 +6,30 @@ import java.util.Scanner;
                 Scanner scannerNumber = new Scanner(System.in);
                 Scanner scannerString = new Scanner(System.in);
 
-                System.out.println("");
-                =scannerNumber.nextInt()
+                System.out.println("Insira o nome do primeiro funcionario:");
+                String funcionario1 = scannerString.nextLine();
+
+                System.out.println("Insira o salario do primeiro funcionario:");
+                int salario1 = scannerNumber.nextInt();
+
+                System.out.println("Insira o nome do segundo funcionario:");
+                String funcionario2 = scannerString.nextLine();
+
+                System.out.println("Insira o salario do segundo funcionario:");
+                int salario2 = scannerNumber.nextInt();
+
+                System.out.println("Insira o nome do terceiro funcionario:");
+                String funcionario3 = scannerString.nextLine();
+
+                System.out.println("Insira o salario do terceiro funcionario:");
+                int salario3 = scannerNumber.nextInt();
+
+                int somaSalarios = (salario1 + salario2 + salario3);
+                int mediaSalario = (somaSalarios/3) ;
+
+                System.out.println("A media dos salários de:"+funcionario1+", "+funcionario2+"e "+funcionario3+" foi de: "+mediaSalario);
+
+
             }
 
         }
